f2389808f5d4f8acfd930366206cd802bfaa20af
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-03-25 20:08:21 +0700

    3. Tính năng lọc theo trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,14 +2,45 @@ const Product = require("../../models/product.model");
 
 // [GET] /admin/products/
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
+  const find = {
     deleted: false
-  });
+  };
 
-  console.log(products);
+  const filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: ""
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: ""
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: ""
+    }
+  ];
+
+  if(req.query.status) {
+    const index = filterStatus.findIndex(item => item.status == req.query.status);
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex(item => item.status == "");
+    filterStatus[index].class = "active";
+  }
+
+  if(req.query.status) {
+    find.status = req.query.status;
+  }
+
+  const products = await Product.find(find);
 
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
-    products: products
+    products: products,
+    filterStatus: filterStatus
   });
 }
