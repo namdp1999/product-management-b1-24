88f20c6898a44638ad1daa8b0963566061812491
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-03-27 20:17:05 +0700

    2. Tính năng phân trang - Tối ưu code

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../../models/product.model");
 const filterHelper = require("../../helpers/filter.helper");
+const paginationHelper = require("../../helpers/pagination.helper");
 
 // [GET] /admin/products/
 module.exports.index = async (req, res) => {
@@ -23,19 +24,8 @@ module.exports.index = async (req, res) => {
   // End Search
 
   // Pagination
-  const objectPagination = {
-    currentPage: 1,
-    limitItems: 4
-  }
-
-  if(req.query.page) {
-    objectPagination.currentPage = parseInt(req.query.page);
-  }
-
-  objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems;
-
   const countRecords = await Product.countDocuments(find);
-  objectPagination.totalPage = Math.ceil(countRecords/objectPagination.limitItems);
+  const objectPagination = paginationHelper(req, countRecords);
   // End Pagination
 
   const products = await Product
