227c990f238fd5315839d66a5faf2f8a24fa857d
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-03-27 19:10:40 +0700

    1.1. Tối ưu phần Bộ lọc

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model");
+const filterHelper = require("../../helpers/filter.helper");
 
 // [GET] /admin/products/
 module.exports.index = async (req, res) => {
@@ -7,31 +8,7 @@ module.exports.index = async (req, res) => {
   };
 
   // Filter
-  const filterStatus = [
-    {
-      name: "Tất cả",
-      status: "",
-      class: ""
-    },
-    {
-      name: "Hoạt động",
-      status: "active",
-      class: ""
-    },
-    {
-      name: "Dừng hoạt động",
-      status: "inactive",
-      class: ""
-    }
-  ];
-
-  if(req.query.status) {
-    const index = filterStatus.findIndex(item => item.status == req.query.status);
-    filterStatus[index].class = "active";
-  } else {
-    const index = filterStatus.findIndex(item => item.status == "");
-    filterStatus[index].class = "active";
-  }
+  const filterStatus = filterHelper(req);
 
   if(req.query.status) {
     find.status = req.query.status;
