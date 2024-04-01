6cbd2b7403e14bd0de74618ec179fe9f493dde9f
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-04-01 19:59:41 +0700

    2. Tính năng thay đổi thứ tự sản phẩm

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -2,10 +2,12 @@ const Product = require("../../models/product.model");
 
 // [GET] /products/
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
-    status: "active",
-    deleted: false
-  });
+  const products = await Product
+    .find({
+      status: "active",
+      deleted: false
+    })
+    .sort({ position: "desc" });
 
   for (const item of products) {
     item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0);
