03741f55341b23456f9dfad6e73b04dc7ae577de
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-04-08 19:38:20 +0700

    6. Đẩy file tĩnh lên Cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -2,8 +2,11 @@ const express = require("express");
 const multer  = require('multer');
 const router = express.Router();
 
-const storage = require("../../helpers/storageMulter.helper");
-const upload = multer({ storage: storage });
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+
+// const storage = require("../../helpers/storageMulter.helper");
+// const upload = multer({ storage: storage });
+const upload = multer();
 
 const controller = require("../../controllers/admin/product.controller");
 const validate = require("../../validates/admin/product.validate");
@@ -21,6 +24,7 @@ router.get("/create", controller.create);
 router.post(
   "/create",
   upload.single('thumbnail'),
+  uploadCloud.uploadSingle,
   validate.createPost,
   controller.createPost
 );
@@ -30,6 +34,7 @@ router.get("/edit/:id", controller.edit);
 router.patch(
   "/edit/:id",
   upload.single('thumbnail'),
+  uploadCloud.uploadSingle,
   validate.createPost,
   controller.editPatch
 );
