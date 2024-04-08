1b988442a5ca81606a24b33fe505f0611bafcacf
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-04-08 20:38:44 +0700

    2. TinyMCE (Tích hợp TinyMCE)

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const flash = require('express-flash');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const path = require('path');
 dotenv.config();
 
 database.connect();
@@ -35,6 +36,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // App Locals Variable
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
+/* New Route to the TinyMCE Node module */
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
 
 // Routes
 routeAdmin(app);
