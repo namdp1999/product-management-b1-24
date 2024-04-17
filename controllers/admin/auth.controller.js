e2d91cd3d3a55b854f5e682a56f4961e59d8b111
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-04-17 19:26:13 +0700

    2. Làm tính năng đăng nhập (Kiểm tra dữ liệu gửi lên đúng hay sai)

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -1,3 +1,8 @@
+const md5 = require("md5");
+const Account = require("../../models/account.model");
+
+const systemConfig = require("../../config/system");
+
 // [GET] /admin/auth/login
 module.exports.login = async (req, res) => {
   res.render("admin/pages/auth/login", {
@@ -7,7 +12,32 @@ module.exports.login = async (req, res) => {
 
 // [POST] /admin/auth/login
 module.exports.loginPost = async (req, res) => {
-  console.log(req.body);
+  const email = req.body.email;
+  const password = req.body.password;
 
-  res.send("OK");
+  const user = await Account.findOne({
+    email: email,
+    deleted: false
+  });
+
+  if(!user) {
+    req.flash("error", "Email không tồn tại!");
+    res.redirect("back");
+    return;
+  }
+
+  if(md5(password) != user.password) {
+    req.flash("error", "Sai mật khẩu!");
+    res.redirect("back");
+    return;
+  }
+
+  if(user.status != "active") {
+    req.flash("error", "Tài khoản đang bị khóa!");
+    res.redirect("back");
+    return;
+  }
+
+  res.cookie("token", user.token);
+  res.redirect(`/${systemConfig.prefixAdmin}/dashboard`);
 };
