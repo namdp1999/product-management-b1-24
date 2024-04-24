4ed1642838e548d00eb1740f42bd51d417fb3077
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-04-24 20:14:16 +0700

    2.3. Làm trang giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,33 @@
 const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
+
+// [GET] /cart/
+module.exports.index = async (req, res) => {
+  const cart = await Cart.findOne({
+    _id: req.cookies.cartId
+  });
+
+  cart.totalPrice = 0;
+
+  for (const item of cart.products) {
+    const infoProduct = await Product.findOne({
+      _id: item.product_id
+    }).select("thumbnail title price discountPercentage stock slug");
+
+    infoProduct.priceNew = (infoProduct.price * (100 - infoProduct.discountPercentage)/100).toFixed(0);
+
+    infoProduct.totalPrice = infoProduct.priceNew * item.quantity;
+
+    cart.totalPrice += infoProduct.totalPrice;
+
+    item.infoProduct = infoProduct;
+  }
+
+  res.render("client/pages/cart/index", {
+    pageTitle: "Giỏ hàng",
+    cartDetail: cart
+  });
+};
 
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
