362cf0b93f3208013bf58fc85cde1d1baacc4c4b
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-04-24 19:46:05 +0700

    2.1. Thêm sản phẩm vào giỏ hàng

---
+++ middlewares/client/cart.middleware.js
@@ -0,0 +1,11 @@
+const Cart = require("../../models/cart.model");
+
+module.exports.cart = async (req, res, next) => {
+  if(!req.cookies.cartId) {
+    const cart = new Cart();
+    await cart.save();
+
+    res.cookie("cartId", cart.id);
+  }
+  next();
+}
