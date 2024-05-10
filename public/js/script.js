f9d01c378bd9fc907f33d4e70ab0bb37a2eea49c
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-05-10 20:20:02 +0700

    5.2. Làm tính năng chat cơ bản

--- public/js/script.js
+++ public/js/script.js
@@ -31,3 +31,18 @@ if(tableCart) {
   });
 }
 // End Update Cart
+
+// CLIENT_SEND_MESSAGE
+const formSendData = document.querySelector(".chat .inner-form");
+if(formSendData) {
+  formSendData.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const content = formSendData.content.value;
+    
+    if(content) {
+      socket.emit("CLIENT_SEND_MESSAGE", content);
+      formSendData.content.value = "";
+    }
+  })
+}
+// End CLIENT_SEND_MESSAGE
